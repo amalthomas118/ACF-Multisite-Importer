dc658203e368b1981455d6e82a7965276be74bb8
Amal Thomas <amal.thomas@phases.io>
2024-07-18 10:10:12 +0530

    Minor change

--- multisite-acf-importer.php
+++ multisite-acf-importer.php
@@ -15,8 +15,6 @@
  Domain Path: /languages
  
 ------------------------------------------------------------------------
-Copyright 2009-2024 Phases, Inc.
-
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
